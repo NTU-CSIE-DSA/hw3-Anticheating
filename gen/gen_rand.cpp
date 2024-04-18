7744cbe078afe78af6194133a70c0c368d4f2267
yungyaoc <yycheng910@gmail.com>
2024-04-18 20:42:38 +0800

    add some more tests

--- gen/gen_rand.cpp
+++ gen/gen_rand.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(int argc, char* argv[]){
     registerGen(argc, argv, 1);
 
-    int slen = atoi(argv[1]), tlen = atoi(argv[2]);
-    cout << jngen::StringRandom::random(slen) << '\n' << jngen::StringRandom::random(tlen) << '\n';
+    int slen = atoi(argv[1]), tlen = atoi(argv[2]), dict_size = atoi(argv[3]);
+    assert(1 <= dict_size && dict_size <= 26);
+    int st = rnd.next(27 - dict_size);
+    string dict = string(1, 'a' + st) + "-" + string(1, 'a' + st + dict_size - 1);
+    cout << jngen::StringRandom::random(slen, dict) << '\n' << jngen::StringRandom::random(tlen, dict) << '\n';
 }
--- prob.md
+++ prob.md
@@ -5,25 +5,42 @@ Please refer to the PDF on NTU COOL.
 
 ### Sample Input 1
 ```
+aaaaa
+aaa
 ```
 
 ### Sample Output 1
 ```
+5
+8
+9
 ```
 
 ### Sample Input 2
 ```
+ntucsiedsa
+csie
 ```
 
 
 ### Sample Output 2
 ```
+1
+1
+1
+1
 ```
 
 ### Sample Input 3
 ```
+baabaab
+baab
 ```
 
 ### Sample Output 3
 ```
+3
+2
+2
+4
 ```

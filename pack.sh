32dcd0d9ea337ad8eee41546b6571b329223fba4
yungyaoc <yycheng910@gmail.com>
2024-04-23 00:00:22 +0800

    Re-Finalize the problem

--- pack.sh
+++ pack.sh
@@ -5,7 +5,7 @@ cp prob.md ./package/prob.md
 cp ./tests/*.in ./package/testdata/
 cp ./tests/*.out ./package/testdata/
 g++ ./packdata.cpp -o packdata.exe
-./packdata.exe 2 262144 0 10 10 20 60
+./packdata.exe 3 262144 0 10 10 20 60
 
 rm -f ./package.tar.gz
 tar zcvf package.tar.gz package
